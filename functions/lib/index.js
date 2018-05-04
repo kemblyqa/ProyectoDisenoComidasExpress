93ee049795ac5216b31cda3aaad5aee4e78437c9
Alberth Salas C <alberthsalascalero@gmail.com>
2018-05-04 17:54:49 -0600

    FireBase Configurado

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,9 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    if (request.method == "POST") {
+        admin.firestore().collection('Restaurantes').add(request.body).then(writeResult => {
+            // write is complete here
+        });
+    }
+    response.send({ msg: "Hello from Firebase!" });
+});
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
 import * as functions from 'firebase-functions';
 
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+    if (request.method=="POST"){
+        admin.firestore().collection('Restaurantes').add(request.body).then(writeResult => {
+            // write is complete here
+        });
+    }
+    response.send({msg:"Hello from Firebase!"});
+});
