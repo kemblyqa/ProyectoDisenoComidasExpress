0eae2fb87354cad3a4a31e5d1c1a083d433f91f2
Kembly Quirós Araya <kquirosaraya@gmail.com>
2018-05-08 09:39:52 -0500

    Children (Menu) Parent (Dashboard)

--- AngularApp/src/app/app.routing.ts
+++ AngularApp/src/app/app.routing.ts
@@ -1,14 +1,23 @@
-import { MenuRestaurantComponent } from './manager/menu-restaurant/menu-restaurant.component';
+import { MainPageComponent } from './main/main-page/main-page.component';
+import { MenuRestaurantComponent } from './main/menu-restaurant/menu-restaurant.component';
 import { Routes } from "@angular/router";
 
 export const ROUTES: Routes = [
     {
-        path: '', redirectTo: 'menu', pathMatch: 'full'
+        path: '', redirectTo: 'dashboard', pathMatch: 'full'
     },
     {
-        path: 'menu', component: MenuRestaurantComponent
+        path: 'dashboard', component: MainPageComponent,
+        children: [
+            {
+                path: 'menu', component: MenuRestaurantComponent
+            },
+            {
+                path: '', redirectTo: 'dashboard', pathMatch: 'full'
+            }
+        ]
     },
     {
-        path: '**', redirectTo: 'menu', pathMatch: 'full'
+        path: '**', redirectTo: 'dashboard', pathMatch: 'full'
     }
 ]
