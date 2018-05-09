4e9935beca987d3085ddd321f9e66ffef07c10a4
Alberth Salas C <alberthsalascalero@gmail.com>
2018-05-08 23:06:40 -0600

    enpoint para añadir platillos

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -34,13 +34,13 @@ export const categoria = functions.https.onRequest((request, response) => {
 export const filtroPlat = functions.https.onRequest((request, response) => {
     if (request.method=="GET"){
         let categoria = request.query.categoria
-        let keyRes = request.query.keyRes
+        let keyRest = request.query.keyRest
         let nombre = request.query.nombre
         let query = platillos
         if(categoria!=undefined)
             query=query.where('categoria','==',categoria)
-        if(keyRes!=undefined)
-            query=query.where('restaurante','==',keyRes)
+        if(keyRest!=undefined)
+            query=query.where('restaurante','==',keyRest)
         if(nombre!=undefined)
             query=query.where('nombre','==',nombre)
         query.get()
@@ -79,3 +79,28 @@ export const filtroPlat = functions.https.onRequest((request, response) => {
     else
         response.send({status:false,data:'Solicitud desconocida'});
 });
+
+export const addPlatillo = functions.https.onRequest((request, response) => {
+    if (request.method='POST'){
+        let descripcion = request.body.descripcion
+        let imagen = request.body.imagen
+        let keyRest = request.body.keyRest
+        let categoria = request.body.categoria
+        let nombre = request.body.nombre
+        if (descripcion==undefined || imagen==undefined||keyRest==undefined||categoria==undefined||nombre==undefined)
+            response.send({status:false,data:"Falta un dato"})
+        platillos.where('restaurante','==',keyRest).where('nombre','==',nombre).get().then(snapshot => {
+            if(!snapshot.empty)
+                response.send({status:false,data:"Este platillo ya existe en este restaurante"})
+                restaurantes.doc(keyRest).get().then(snapshot2 =>{
+                    if(!snapshot2.exists)
+                        response.send({status:false,data:"El restaurante no existe"})
+                    platillos.add({restaurante:keyRest,imagen:imagen,categoria:categoria,descripcion:descripcion,nombre:nombre}).then(ref =>{
+                        response.send({status:true,data:ref.id})
+                })
+            }).catch(err=>{response.send({status:false,data:"Error insertando platillo"})})
+        }).catch(err=>{response.send({status:false,data:"Error insertando platillo"})})
+    }
+    else
+        response.send({status:false,data:'Metodo no encontrado'})
+})
